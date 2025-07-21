90d52907ae514832a99a05e11515beab776e7ed2
Hillzzzz <163678865+Hillzzzz@users.noreply.github.com>
2025-07-21 10:45:37 +0300

    Update vite.config.mjs

--- music-player/vite.config.mjs
+++ music-player/vite.config.mjs
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // important for Vercel
+  root: '.', // Important if your index.html is in root
+  build: {
+    outDir: 'dist', // default
+  },
 });
